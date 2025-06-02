a9dff60a66fb449887a1e6ee8fd688218a54e085
ProgramadorVictor <victorochaandrade@gmail.com>
2025-06-01 22:29:52 -0300

    adicionando o vue router ao projeto

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+//vue add router //plugin: instala e configura automaticamente
+//npm i --save -E vue-router // --save (salvar no package) --E (instalar exatamente a versão definida)
