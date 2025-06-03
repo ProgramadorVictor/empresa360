6dc7fd5b0bcfa115e59deae4d325b31872fb7d45
ProgramadorVictor <victorochaandrade@gmail.com>
2025-06-02 21:29:11 -0300

    !configurando o vue router, conceitos e dicas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view/> <!-- Pós definição, de todo processo em main.js, usamos o router-view, para renderizar os componentes para serem exibidos -->
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Inicio das rotas
+import HomeComponent from './views/HomeComponent.vue'
+import LoginComponent from './views/LoginComponent.vue'
+import { createRouter, createWebHashHistory } from 'vue-router' //Importando a função createRouter.
+
+const routes = [ //Criação de todas as rotas do Vue Router.
+    {
+        path: '/home', //Caminho para o componente 'HomeComponent' é acessado por /home. localhost:8080/home
+        component: HomeComponent
+    },
+    {
+        path: '/login', //localhost:8080/login
+        component: LoginComponent
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(), //Atribuindo o retorno da função createWebHashHistory() ao history.
+    // routes: routes //passando o const routes, para criação de rotas
+    routes //se a chave possui o mesmo nome cuja o valor seja atribuido a chave, tenha o mesmo nome, podemos ocultar a atribuição.
+    //Ao invés de usar 'routes: routes', pode ser usado 'routes'
+})
+
+const Vue = createApp(App)
+Vue.use(router) //Adicionando as configurações de rota à instância do Vue.
+Vue.mount('#app') //Pós configuração e montagem, colocamos o '<router-view/>', para renderizar o componente.
+
+// Fim das rotas
 
 //vue add router //plugin: instala e configura automaticamente
 //npm i --save -E vue-router // --save (salvar no package) --E (instalar exatamente a versão definida)
