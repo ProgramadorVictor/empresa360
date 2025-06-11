9b773f38a8152e654c74e59269ad938b7249ba58
ProgramadorVictor <victorochaandrade@gmail.com>
2025-06-10 21:45:11 -0300

    router link navegação entre rotas

--- src/views/HomeComponent.vue
+++ src/views/HomeComponent.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
+        <!-- Não é criado uma requisição http caso utilize a SPA. -->
         Home
+        <router-link :to="{path: '/login'}">Navegar para "/login"</router-link>
+        <!-- tag <router-link> utilizado para indicar uma navegação para outra página, porém o to="" é essencial para escolher a página. -->
     </div>
 </template>
 <script>
--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
+        <!-- Não é criado uma requisição http caso utilize a SPA. -->
         Login
+        <router-link to="/home">Navegar para "/home"</router-link>
+        <!-- tag <router-link> utilizado para indicar uma navegação para outra página, porém o to="" é essencial para escolher a página. -->
     </div>
 </template>
 <script>
