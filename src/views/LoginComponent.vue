d194a291aa8f9efa941faeeb764b259b7f99a483
ProgramadorVictor <victorochaandrade@gmail.com>
2025-06-17 21:22:23 -0300

    navegação programática objeto $router, métodos push e replace

--- src/views/LoginComponent.vue
+++ src/views/LoginComponent.vue
@@ -26,10 +26,27 @@
                         <div class="mb-3">
                             <input type="password" class="form-control" placeholder="Senha">
                         </div>
-                        <button class="btn btn-primary">Login</button>
+                        <button class="btn btn-primary" @click="navegarPara()">Login</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        methods:{
+            navegarPara(){
+                this.$router.replace('/home'); //Replace substituir as rotas, sem criar um histórico de navegação. Ele modifica a URL atual.
+                // this.$router.replace({path: '/home'}); //Outra forma de definir o replace
+
+                console.log(this.$router); //Objeto de rastreamento
+                // this.$router.push('/home'); //Adicionar uma entrada ao histórico de rotas do navegador.
+
+                //Após o disparo do evento ao clicar em login, você é enviado para rota '/home'.
+
+                // this.$router.push({path: '/home'}); //Outra forma de definir uma navegação diferente de this.$router.push('/home');
+            }
+        }
+    }
+</script>
