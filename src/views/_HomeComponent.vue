82d0c9117f172d3c4ba0b6849a1582d40706f5da
ProgramadorVictor <victorochaandrade@gmail.com>
2025-06-20 20:36:13 -0300

    implementando a pagina home

---
+++ src/views/_HomeComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- Não é criado uma requisição http caso utilize a SPA. -->
+        Home
+        <router-link :to="{path: '/'}">Sair</router-link>
+        <!-- tag <router-link> utilizado para indicar uma navegação para outra página, porém o to="" é essencial para escolher a página. -->
+    </div>
+</template>
+<script>
+    export default {
+    
+    }
+</script>
+<style lang="css">
+</style>
