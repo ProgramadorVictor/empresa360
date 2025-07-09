a5fc8dbe61534e0fee55820fc502e90845f1e01d
ProgramadorVictor <victorochaandrade@gmail.com>
2025-07-08 22:13:23 -0300

    instalação do pacote json-server

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <router-view/> <!-- Pós definição, de todo processo em main.js, usamos o router-view, para renderizar os componentes para serem exibidos -->
 </template>
 <script>
+/*
+  npm install -g json-server@v0.17.0 -E //Somente instalação do json-server
+  json-server -h
+*/
 export default {
   name: 'App'
 }
