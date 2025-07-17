c93312c9a5c2e925967fc1918884ac86dd71c500
ProgramadorVictor <victorochaandrade@gmail.com>
2025-07-16 21:17:14 -0300

    requisição http para api fake com response

--- src/components/vendas/LeadsComponent.vue
+++ src/components/vendas/LeadsComponent.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
         <h5>Leads</h5>
+        {{ dados }}
     </div>
 </template>
 <script>
     export default {
-    
+        name: 'LeadsComponent',
+        data: () => ({
+            dados: null
+        }),
+        methods: {
+            getDadosApi(){
+                let url = 'http://localhost:3000/leads'
+                fetch(url)
+                    .then(response => response.json())
+                    .then(response => {
+                        this.dados = response
+                    })
+            }
+        },
+        created(){
+            this.getDadosApi();
+        }
     }
 </script>
 <style lang="css">
